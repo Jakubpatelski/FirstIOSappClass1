4ff52ac26a4605f1065240535d694651c5fbf393
Jakubpatelski <jakubpatelski24@gmail.com>
2023-02-01 10:54:23 +0100

    com

--- FirstIOSapp/ContentView.swift
+++ FirstIOSapp/ContentView.swift
@@ -9,15 +9,95 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+
+        
+        VStack (spacing: 15){
+            
+            Text("Business card")
+                .font(.system(size: 36))
+                .font(.title)
+                .fontWeight(.medium)
+                
+                .foregroundColor(Color.blue)
+                .multilineTextAlignment(.leading)
+            
+            Image("maersklogo")
+                .resizable()
+                .edgesIgnoringSafeArea(.top)
+                .padding(0.0)
+                .scaledToFit()
+                
+                .frame(height: 200)
+                .clipped()
+                .cornerRadius(20)
+            
+            
+            VStack (spacing: 10){
+                Text("Jakub Patelski")
+                   
+                Text("Student Assistant")
+            }
+            .font(.title)
+            
+            
+                
+                HStack {
+                    Image(systemName: "phone.fill")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("+45 22 44 22 44")
+                        .font(.subheadline)
+                    
+                }
+            Divider()
+
+                HStack {
+                    
+                    Image(systemName: "envelope.fill")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("jakub122@gmail.com")
+                        .font(.subheadline)
+                    
+                }
+            Divider()
+            HStack {
+                
+                Image(systemName: "house.fill")
+                    .font(.subheadline)
+                Spacer()
+                Text("Copnehagen K")
+                    .font(.subheadline)
+                
+            }
+            
+
+            Spacer()
+            
+            VStack {
+                Image("maersk2")
+                    .resizable()
+                    .cornerRadius(10)
+                    .shadow(radius: 7)
+
+                    
+                  
+                
+            }
+
         }
         .padding()
+
+       
+       
+
+            
+        }
+
+    
+
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
